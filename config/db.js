f7e91a06ffc53ffd20d7b31681d22192dff2bcea
Your Name <you@example.com>
2021-11-29 16:03:29 -0300

    commit con firebase funcionando

--- config/db.js
+++ config/db.js
@@ -34,31 +34,34 @@ const dbfirebase = admin.firestore();
 
 // EXPORTANDO LAS BASES DE DATOS
 
-module.exports = { mysql, sqlite, dbfirebase };
+module.exports = { 
+  // mysql, 
+  // sqlite, 
+  dbfirebase };
 
 // CREACION DE TABLAS
 
-mysql.schema.hasTable("productos").then(function (exists) {
-    if (!exists) {
-      return mysql.schema.createTable("productos", (table) => {
-        table.increments('id_productos').primary();
-        table.string("name", 128).notNullable()
-        table.string("description", 1000).notNullable()
-        table.integer("price", 100).notNullable()
-        table.string("thumbnail", 1000).notNullable()
-        table.integer("stock", 100).notNullable()
-      });
-    }
-  });
+// mysql.schema.hasTable("productos").then(function (exists) {
+//     if (!exists) {
+//       return mysql.schema.createTable("productos", (table) => {
+//         table.increments('id_productos').primary();
+//         table.string("name", 128).notNullable()
+//         table.string("description", 1000).notNullable()
+//         table.integer("price", 100).notNullable()
+//         table.string("thumbnail", 1000).notNullable()
+//         table.integer("stock", 100).notNullable()
+//       });
+//     }
+//   });
 
-mysql.schema.hasTable("carritos").then(function (exists) {
-    if (!exists) {
-      return mysql.schema.createTable("carritos", (table) => {
-        table.increments(); 
-        table.integer("id_carrito", 255).notNullable()
-        table.integer("id_producto", 128).notNullable()
-        table.integer("cantidad", 128).notNullable()
-      });
-    }
-});
+// mysql.schema.hasTable("carritos").then(function (exists) {
+//     if (!exists) {
+//       return mysql.schema.createTable("carritos", (table) => {
+//         table.increments(); 
+//         table.integer("id_carrito", 255).notNullable()
+//         table.integer("id_producto", 128).notNullable()
+//         table.integer("cantidad", 128).notNullable()
+//       });
+//     }
+// });
 
--- controllers Firebase/controllers.productos.js
+++ controllers Firebase/controllers.productos.js
@@ -22,23 +22,48 @@ const write = async (req, res) => {
     }
 }
 
-// NO ESTA CHECKEADO ESTO
 const read = async (req, res) => {
 
     try{
         const result = []
-        const snapshot = await this.coleccion.get();
+        const snapshot = await db.collection('productos').get();
         snapshot.forEach(doc => {
-            result.push({ id: doc.id, ...doc.data() })
+            result.push({ id: doc.id, ...doc.data()})
             })
-        return result
+        console.log(result)
+        res.json(result)
     }
     catch(err){
-        console.log("ESTE ES EL ERROR", err)
+        console.log("Error desde READ", err)
+    }
+}
+
+// HAY QUE INCLUIR EN EL JSON el ID Que se quiere modificar de la tabla de firebase ( POR POSTMAN)
+
+const update = async (req, res) => {
+    try {
+        console.log(req.body.id)
+        const actualizado = await db.collection('productos').doc(req.body.id).set(req.body);
+        res.json(actualizado)
+    } catch (error) {
+        throw new Error(`Error al actualizar: ${error}`)
+    }
+}
+
+// HAY QUE ENVIAR EN EL JSON el ID ( POR POSTMAN)
+
+const deleted = async (req,res) => {
+    try {
+        const item = await db.collection('productos').doc(req.body.id).delete();
+        res.json(`producto con el id ${req.body.id} eliminado`)
+    } catch (error) {
+        throw new Error(`Error al borrar: ${error}`)
     }
 }
 
 module.exports = {
     write,
-    read
+    read,
+    update,
+    deleted
 }
